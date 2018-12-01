a21a146e541c688ce0ef9d2a69b3d318215e7b06
Adam <geekosoft@live.co.uk>
2018-12-01 23:52:30 +0000

    Add docs/ for GitHub pages

---
+++ docs/precache-manifest.c296d1d502d95074039045cc1908268f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d17100b63b480e13318710c3d09f110",
+    "url": "/static/media/image.6d17100b.png"
+  },
+  {
+    "revision": "74a65497e275c0f7d219b0952ac1c900",
+    "url": "/static/media/document.74a65497.png"
+  },
+  {
+    "revision": "6fce31c95747a88a485c48343c2093eb",
+    "url": "/static/media/cross.6fce31c9.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7c036e6ea0df773bd9e3",
+    "url": "/static/js/main.7c036e6e.chunk.js"
+  },
+  {
+    "revision": "a9e77ad94bbc40792fbd",
+    "url": "/static/js/1.a9e77ad9.chunk.js"
+  },
+  {
+    "revision": "c5080b9302281314322a0780cff73cf1",
+    "url": "/index.html"
+  }
+];
