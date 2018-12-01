06db0233d28ab592939ba9aaa1c620ddc7cdb961
Adam <geekosoft@live.co.uk>
2018-12-01 23:58:48 +0000

    Updates

---
+++ precache-manifest.574d587504b972e7a6b23ad589ef4b65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d17100b63b480e13318710c3d09f110",
+    "url": "/covalent-zip/static/media/image.6d17100b.png"
+  },
+  {
+    "revision": "74a65497e275c0f7d219b0952ac1c900",
+    "url": "/covalent-zip/static/media/document.74a65497.png"
+  },
+  {
+    "revision": "6fce31c95747a88a485c48343c2093eb",
+    "url": "/covalent-zip/static/media/cross.6fce31c9.png"
+  },
+  {
+    "revision": "45833cd34fc4ae9abe36",
+    "url": "/covalent-zip/static/js/runtime~main.45833cd3.js"
+  },
+  {
+    "revision": "ea6101aacf19b18e40b7",
+    "url": "/covalent-zip/static/js/main.ea6101aa.chunk.js"
+  },
+  {
+    "revision": "dd53b875ccfc1db92f9e",
+    "url": "/covalent-zip/static/js/1.dd53b875.chunk.js"
+  },
+  {
+    "revision": "53328b22de8597785ebd7b340bbba1be",
+    "url": "/covalent-zip/index.html"
+  }
+];
